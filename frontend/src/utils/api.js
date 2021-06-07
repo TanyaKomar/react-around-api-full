443d01ef02a42587bf00d4d4eeacece9f1f2ffcb
Tanya Komar <tatiana23komar@gmail.com>
2021-06-07 13:53:58 -0700

    fix/baseURL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,5 @@
-// const BASE_URL = "https://api.tanyakomar.students.nomoreparties.site";
-export const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://api.tanyakomar.students.nomoreparties.site";
+//const BASE_URL = "http://localhost:3000";
 
 class Api {
   constructor({ baseUrl, headers }) {
--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,6 +1,5 @@
-// import BASE_URL  from "./api";
-// const BASE_URL = "https://api.tanyakomar.students.nomoreparties.site";
-export const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://api.tanyakomar.students.nomoreparties.site";
+// const BASE_URL = "http://localhost:3000";
 export const register = (email, password) => {
   return fetch(`${BASE_URL}/signup`, {
     method: "POST",
